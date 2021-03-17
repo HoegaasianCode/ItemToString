e5056dba6cf8ab014737824ea08eb3e08ee9e9a9
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:44:16 +0100

    Initial commit

---
+++ ItemsToString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ItemsToString
+{
+    // https://edabit.com/challenge/hqTYj7NbLnCcjxryi
+    // SOLVED
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new StringConverter(new object[] { 1, 2, "a", "b" });
+            var stringItems = items.ConvertToString();
+            foreach(var stringItem in stringItems)
+            {
+                Console.WriteLine(stringItem);
+            }
+        }
+    }
+}
---
+++ ItemsToString/StringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemsToString
+{
+
+
+    public class StringConverter
+    {
+        private readonly object[] _objectArray;
+
+        public StringConverter(object[] objectArray)
+        {
+            _objectArray = objectArray;
+        }
+
+        public string[] ConvertToString()
+        {
+            var stringList = new List<string>();
+            foreach (var element in _objectArray)
+            {
+                stringList.Add(element.ToString());
+            }
+            return stringList.ToArray();
+        }
+    }
+}
